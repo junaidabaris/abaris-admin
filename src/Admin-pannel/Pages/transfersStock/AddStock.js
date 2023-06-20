513b0b622b7b8d40e7c191e2ba3bb6426f8c3e1c
junaid akhtar1 <bewithjuniad@gmail.com>
2023-06-20 19:00:18 +0530

    done works

--- src/Admin-pannel/Pages/transfersStock/AddStock.js
+++ src/Admin-pannel/Pages/transfersStock/AddStock.js
@@ -1,18 +1,50 @@
-import { useEffect, useState } from "react"
-import { useAddPurchaseCartMutation, useDeleteCartRowMutation, useGetPickupPointQuery, useGetProductSearchQuery } from "../../Components/all-products/allproductsApi/allProductsApi"
+import { useEffect, useRef, useState } from "react"
+import { useAddPurchaseCartMutation, useDeleteCartRowMutation, useGetPickupPointQuery, useGetProductSearchQuery, useGetSellersQuery, useUpdateListProductsMutation } from "../../Components/all-products/allproductsApi/allProductsApi"
 import { RxCross1 } from "react-icons/rx"
 import GenerateTr from "../../Components/addPurchaseList/GenerateTr"
+import { AiFillDelete } from "react-icons/ai"
+import axios from "axios"
+import GenerateStock from "./GenerateStock"
 
 function AddStock() {
     const [show, setShow] = useState(false)
     const [searchs, setSearch] = useState('')
     const { data: searchPro } = useGetProductSearchQuery(searchs)
     const { data: pickUp } = useGetPickupPointQuery();
+    const { data: sellerD } = useGetSellersQuery()
 
-    const [setCart, { isLoading, data: cartData, isError: isCartsError }] = useAddPurchaseCartMutation()
+    const showMainadmin = window.localStorage.getItem('showMainadmin')
+    const isSellerLogin = window.localStorage.getItem('isSellerLogin')
+
+    let ids;
+
+    if (showMainadmin === 'true') {
+        ids = window.localStorage.getItem('adminId')
+    }
+    if (isSellerLogin === 'true') {
+        ids = window.localStorage.getItem('isSellerId')
+    }
+
+    const [state, setState] = useState({
+        FrompickupPoints: "",
+        TopickupPoints: "",
+        supplier: "",
+        shippingCost: "",
+        staff_id: ids,
+        date: "",
+        referenceNo: "",
+        products: []
+    })
+
+    const [setCart, { isLoading, data: cartData, isError: isCartsError, isSuccess: isCartssuss }] = useAddPurchaseCartMutation()
 
     const [showData, setShowData] = useState([])
-    const [finalCatD, setFinalCatD] = useState([]);
+    useEffect(() => {
+        if (isCartssuss) {
+            setShowData(cartData)
+        }
+    }, [isCartssuss])
+    const [finalCatD, setFinalCatD] = useState(['6412fbf218fa66a37ed430d3']);
 
     const setTableItem = async (item) => {
         const obj = { purchase: showData, pickupPoints: finalCatD }
@@ -20,24 +52,20 @@ function AddStock() {
         setCart({ id: item._id, data: obj })
         setShow(false)
     }
-    const SetPick = (e)=>{
-        const clone = [...finalCatD]
-        clone.push(e.target.value)
-        setFinalCatD(clone)
-    }
 
-    const [deleteRow, { isLoading: deleteRows, data: resData }] = useDeleteCartRowMutation()
+    
+    const [deleteRow, { isSuccess: deleteRows, data: resData }] = useDeleteCartRowMutation()
 
     useEffect(() => {
         if (deleteRows) {
             setShowData(resData)
         }
-    }, [deleteRows, deleteRows])
+    }, [deleteRows])
 
     const DeleteRow = (index) => {
         deleteRow({ index: index, data: { purchase: showData } })
     }
-
+   
 
     const handelChange = (e) => {
         if (e.key === 'Enter') {
@@ -46,6 +74,46 @@ function AddStock() {
             setShow(true)
         }
     }
+
+
+ 
+
+   
+
+    const onchangeHandle = (e) => {
+        const clone = { ...state }
+        clone[e.target.name] = e.target.value
+        setState(clone)
+    }
+
+
+    const [status, setStatus] = useState()
+    const getData = async () => {
+        try {
+            const res = await axios.get(`http://onlineparttimejobs.in/api/transferStockStatus`)
+            setStatus(res.data)
+        } catch (error) {
+
+        }
+    }
+    useEffect(() => {
+        getData()
+    }, [])
+
+    const sendData = async () => {
+        const mapesData = showData.map((item) => {
+            return { product_id: item?.product_id, variant_id: item?.variant_id, sku: item?.sku, qty: item?.qty }
+        })
+        const finalObj = { ...state, products: mapesData }
+
+        try {
+            const res = await axios.post(`http://onlineparttimejobs.in/api/transferStock/add_TransferStock`, finalObj)
+            alert('Stock Addwd Successfully')
+        } catch (error) {
+            alert('Server Error Stock Not add')
+        }
+    }
+
 
 
     return <div className="aiz-main-content" style={{ width: "1400px", margin: "auto" }}>
@@ -65,46 +133,58 @@ function AddStock() {
                         <div className="row mainaddStc">
                             <div className="col-4">
                                 <label for="basic-url" class="form-label">Date</label>
-                                <input type="file" className="form-control" />
+                                <input type="date" name="date" onChange={onchangeHandle} className="form-control" />
                             </div>
                             <div className="col-4">
                                 <label for="basic-url" class="form-label">Reference</label>
-                                <input type="file" className="form-control" />
+                                <input type="text" name="referenceNo" onChange={onchangeHandle} className="form-control" />
                             </div>
 
                             <div className="col-4">
                                 <label for="basic-url" class="form-label">To Pickup Point *</label>
-                                <select className="form-select" onChange={SetPick} aria-label="Default select example">
+                                <select className="form-select" name="TopickupPoints" onChange={onchangeHandle} aria-label="Default select example">
                                     <option selected>Open this select menu</option>
-                                    {pickUp && pickUp.map((item)=>{
-                                        return  <option value={item._id}>{item?.pickupPoint_name}</option>
+                                    {pickUp && pickUp.map((item) => {
+                                        return <option value={item._id} key={item._id}>{item?.pickupPoint_name}</option>
                                     })}
-                                   
+
                                 </select>
                             </div>
                             <div className="col-4">
                                 <label for="basic-url" class="form-label">Status *</label>
-                                <select className="form-select" aria-label="Default select example">
+                                <select className="form-select" name="statusId" onChange={onchangeHandle} aria-label="Default select example">
                                     <option selected>Open this select menu</option>
-                                    <option value="1">Pending</option>
-                                    <option value="2">Send</option>
+                                    {status && status.map((item, i) => {
+                                        return <option value={item?._id} key={i}>{item?.name}</option>
+                                    })}
+
                                 </select>
                             </div>
 
                             <div className="col-4">
                                 <label for="basic-url" class="form-label">Shipping</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" name="shippingCost" onChange={onchangeHandle} className="form-control" />
                             </div>
-
                             <div className="col-4">
-                                <label for="basic-url" class="form-label">From Pickup Point *</label>
-                                <select className="form-select" aria-label="Default select example">
+                                <label for="basic-url" class="form-label">Supplier</label>
+                                <select className="form-select" name="supplier" onChange={onchangeHandle} aria-label="Default select example">
                                     <option selected>Open this select menu</option>
-                                    <option value="1">One</option>
-                                    <option value="2">Two</option>
-                                    <option value="3">Three</option>
+                                    {sellerD && sellerD.map((item) => {
+                                        return <option value={item._id} key={item._id}>{item.firstname + " " + item.lastname}</option>
+                                    })}
                                 </select>
                             </div>
+                            <div className="col-4">
+                                <label for="basic-url" class="form-label">From Pickup Point *</label>
+                                <select className="form-select" name="FrompickupPoints" onChange={onchangeHandle} aria-label="Default select example">
+                                    <option selected>Open this select menu</option>
+                                    {pickUp && pickUp.map((item) => {
+                                        return <option value={item._id}>{item?.pickupPoint_name}</option>
+                                    })}
+
+                                </select>
+                            </div>
+
 
 
 
@@ -138,13 +218,11 @@ function AddStock() {
                                             <div className="col-12 sku_combination table-responsive form-group" id="sku_combination">
                                                 <table className="table table-bordered physical_product_show">
                                                     <thead>
-
                                                         <tr>
                                                             <td><label className="control-label">#</label></td>
                                                             <td><label className="control-label">Product Name</label></td>
                                                             <td><label className="control-label">Variant</label></td>
                                                             <td><label className="control-label">SKU</label></td>
-                                                            <td><label className="control-label">Pickup Point</label></td>
                                                             <td><label className="control-label">Quantity</label></td>
                                                         </tr>
 
@@ -152,11 +230,9 @@ function AddStock() {
 
                                                     <tbody>
 
-                                                        {cartData && cartData.map((item, i) => {
-                                                            return <GenerateTr showData={showData} setShowData={setShowData} DeleteRow={DeleteRow} key={i} item={item} index={i} pickUp={pickUp}/>
+                                                        {showData && showData.map((item, i) => {
+                                                            return <GenerateStock item={item} i={i} showData={showData} setShowData={setShowData}DeleteRow={DeleteRow}/>
                                                         })}
-
-
 
                                                     </tbody>
 
@@ -172,7 +248,7 @@ function AddStock() {
 
 
                         </div>
-                        <button style={{ margin: "10px 0" }} type="button" class="btn btn-success">Submit</button>
+                        <button style={{ margin: "10px 0" }} onClick={sendData} type="button" class="btn btn-success">Submit</button>
                     </div>
                 </div>
             </div>
